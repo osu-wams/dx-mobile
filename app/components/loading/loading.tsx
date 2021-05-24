6974ce4a46ce19de2023b167e13886a2797fab6d
Josh Gum <revgum@gmail.com>
2021-05-24 13:43:43 -0700

    lint fix

--- app/components/loading/loading.tsx
+++ app/components/loading/loading.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { useWindowDimensions, View, Animated, Easing, Text } from 'react-native'
+import React from 'react';
+import { useWindowDimensions, View, Animated, Easing, Text } from 'react-native';
 // import { LogoCircular } from '../logo-circular/logo-circular'
-import { presets } from './loading.presets'
-import { LoadingProps } from './loading.props'
+import { presets } from './loading.presets';
+import { LoadingProps } from './loading.props';
 
 export function Loading(props: LoadingProps) {
-  const { height, width } = useWindowDimensions()
+  const { height, width } = useWindowDimensions();
   // grab the props
-  const { preset = 'basic', style: styleOverride } = props
+  const { preset = 'basic', style: styleOverride } = props;
 
   // assemble the style
-  const presetToUse = presets[preset] || presets.basic
-  const style = { ...presetToUse, ...styleOverride, ...{ height, width } }
+  const presetToUse = presets[preset] || presets.basic;
+  const style = { ...presetToUse, ...styleOverride, ...{ height, width } };
 
-  const spinValue = new Animated.Value(0)
+  const spinValue = new Animated.Value(0);
   Animated.loop(
     Animated.timing(spinValue, {
       toValue: 1,
@@ -21,8 +21,8 @@ export function Loading(props: LoadingProps) {
       easing: Easing.linear,
       useNativeDriver: true,
     }),
-  ).start()
-  const spin = spinValue.interpolate({ inputRange: [0, 1], outputRange: ['0deg', '360deg'] })
+  ).start();
+  const spin = spinValue.interpolate({ inputRange: [0, 1], outputRange: ['0deg', '360deg'] });
 
   return (
     <View testID="Loading" style={style}>
@@ -30,5 +30,5 @@ export function Loading(props: LoadingProps) {
         <Text style={{ fontSize: 45 }}>Logo</Text>
       </Animated.View>
     </View>
-  )
+  );
 }
