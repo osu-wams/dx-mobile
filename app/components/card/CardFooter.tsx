40e3d47d659229a177fe1b5ddb94c73e24b9c264
Josh Gum <revgum@gmail.com>
2021-07-17 05:38:47 -0700

    fixes footer link conditional alignment

--- app/components/card/CardFooter.tsx
+++ app/components/card/CardFooter.tsx
@@ -1,9 +1,32 @@
-import React, { useContext } from 'react';
+import React, { ReactNode, useContext } from 'react';
 import styled from 'styled-components/native';
 import InfoButton from '../info-button/InfoButton';
 import { spacing } from '@osu-wams/theme';
 import { CardContext } from './Card';
 import { CardCollapseProps } from './card.props';
+
+const CardFooterWrapper = styled.View<CardCollapseProps & { children?: ReactNode[] }>(
+  {
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginTop: 'auto',
+    overflow: 'hidden',
+    padding: spacing.default,
+  },
+  ({ children }) =>
+    children &&
+    children.length > 1 && {
+      alignItems: 'flex-end',
+    },
+  ({ collapsible, collapsed }) =>
+    collapsible && {
+      display: collapsed ? 'none' : 'flex',
+      flex: 1,
+      height: collapsed ? 0 : 'auto',
+      padding: collapsed ? 0 : `${spacing.medium} ${spacing.default}`,
+    },
+);
 
 const CardFooter = ({ ...props }) => {
   const { collapsed, collapsible } = useContext(CardContext);
@@ -17,27 +40,5 @@ const CardFooter = ({ ...props }) => {
     <></>
   );
 };
-
-const CardFooterWrapper = styled.View<CardCollapseProps>(
-  {
-    padding: spacing.default,
-    overflow: 'hidden',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginTop: 'auto',
-    /* If we only have 1 link, align it right
-    'a:only-child': {
-      marginLeft: 'auto',
-    }, */
-  },
-  ({ collapsible, collapsed }) =>
-    collapsible && {
-      flex: 1,
-      height: collapsed ? 0 : 'auto',
-      padding: collapsed ? 0 : `${spacing.medium} ${spacing.default}`,
-      display: collapsed ? 'none' : 'flex',
-    },
-);
 
 export default CardFooter;
