59041d0010f484bfbe809bad14f2a6fe4ffcb401
Josh Gum <revgum@gmail.com>
2021-07-20 11:43:19 -0700

    adds opensans bold as a preset

--- app/theme/fonts/index.ts
+++ app/theme/fonts/index.ts
@@ -1,8 +1,10 @@
-import { useFonts, OpenSans_400Regular } from '@expo-google-fonts/open-sans';
+/* eslint-disable camelcase */
+import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 
 export const initFonts = () => {
   const fonts = useFonts({
     OpenSans: OpenSans_400Regular,
+    OpenSansBold: OpenSans_700Bold,
   });
   return fonts;
 };
