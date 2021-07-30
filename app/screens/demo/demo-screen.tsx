f186f75efa733422e300fb172649c247463bc893
Josh Gum <revgum@gmail.com>
2021-07-30 08:11:22 -0700

    component becoming almost useless now

--- app/screens/demo/demo-screen.tsx
+++ app/screens/demo/demo-screen.tsx
@@ -1,33 +1,11 @@
 import React from 'react';
 import { TextStyle, View, ViewStyle } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { Button, Header, Text, Screen } from '../../components';
-import { color, spacing } from '../../theme';
+import { Text, Screen } from '../../components';
+import { spacing } from '../../theme';
 export const heart = require('./heart.png');
 
 const FULL: ViewStyle = { flex: 1 };
-const CONTAINER: ViewStyle = {
-  backgroundColor: color.transparent,
-  paddingHorizontal: spacing[4],
-};
-const DEMO: ViewStyle = {
-  paddingVertical: spacing[4],
-  paddingHorizontal: spacing[4],
-  backgroundColor: '#5D2555',
-};
 const BOLD: TextStyle = { fontWeight: 'bold' };
-const HEADER: TextStyle = {
-  paddingTop: spacing[3],
-  paddingBottom: spacing[5] - 1,
-  paddingHorizontal: 0,
-};
-const HEADER_TITLE: TextStyle = {
-  ...BOLD,
-  fontSize: 12,
-  lineHeight: 15,
-  textAlign: 'center',
-  letterSpacing: 1.5,
-};
 const TITLE: TextStyle = {
   ...BOLD,
   fontSize: 28,
@@ -35,30 +13,12 @@ const TITLE: TextStyle = {
   textAlign: 'center',
   marginBottom: spacing[5],
 };
-const TAGLINE: TextStyle = {
-  color: '#BAB6C8',
-  fontSize: 15,
-  lineHeight: 22,
-  marginBottom: spacing[4] + spacing[1],
-};
 
 export const DemoScreen = () => {
-  const navigation = useNavigation();
-  const goBack = () => navigation.goBack();
-
   return (
     <View testID="DemoScreen" style={FULL}>
-      <Screen style={CONTAINER} preset="scroll">
-        <Header
-          headerTx="demoScreen.howTo"
-          leftIcon="back"
-          onLeftPress={goBack}
-          style={HEADER}
-          titleStyle={HEADER_TITLE}
-        />
+      <Screen preset="scroll">
         <Text style={TITLE} preset="header" tx="demoScreen.title" />
-        <Text style={TAGLINE} tx="demoScreen.tagLine" />
-        <Button style={DEMO} onPress={() => navigation.navigate('welcome')} />
       </Screen>
     </View>
   );
